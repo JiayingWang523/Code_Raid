f8043f7e341b82be81d2cfe403094836e5622828
Shenggang <hushenggang1996@163.com>
2016-08-06 22:16:18 +0100

    Reconstructed the repository due to some unsolvable conflict.
    Here we have everything again. Logs are lost though.

---
+++ Code_Raid_Prototype_01/Assets/Script/CameraFollow.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+    public GameObject character;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        float x = character.transform.position.x;
+        float y = character.transform.position.y;
+        float z = character.transform.position.z;
+        y = y + 25;
+        z = z - 30;
+        Vector3 v = new Vector3(x, y, z);
+        transform.position = v;
+    }
+}
---
+++ Code_Raid_Prototype_01/Assets/Script/SelfRotate.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SelfRotate : MonoBehaviour {
+
+    public float rotateSpeed = 30.0f;
+
+    private Transform transform;
+
+    // Use this for initialization
+    void Start () {
+        transform = this.GetComponent<Transform>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(transform.up, rotateSpeed * Time.deltaTime);
+	}
+}
